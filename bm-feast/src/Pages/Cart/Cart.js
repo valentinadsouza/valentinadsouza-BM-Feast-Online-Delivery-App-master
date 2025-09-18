ae7244c353cb8c82436f0fe80babb1ac2a0c097e
Valeska <valeskaedu1@gmail.com>
2025-09-18 13:10:37 +0530

    added some more ideas and items

--- bm-feast/src/Pages/Cart/Cart.js
+++ bm-feast/src/Pages/Cart/Cart.js
@@ -12,14 +12,14 @@ const Cart = () => {
   return (
     <div className='cart'>
       <div className='cart-items'>
-        {/* <div className='cart-items-title'>
+        <div className='cart-items-title'>
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
           <p>Quantity</p>
           <p>Total</p>
           <p>Remove</p>
-        </div> */}
+        </div>
         <br />
         <hr />
         {food_list.map((item, index) => {
